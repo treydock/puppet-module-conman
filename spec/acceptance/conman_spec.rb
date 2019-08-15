235e3795ad320d6c1a693622f008e0245763c609
Trey Dockendorf <tdockendorf@osc.edu>
2019-08-15 11:39:35 -0400

    Convert module to PDK and modernize

--- spec/acceptance/conman_spec.rb
+++ spec/acceptance/conman_spec.rb
@@ -2,29 +2,29 @@ require 'spec_helper_acceptance'
 
 describe 'conman class:' do
   context 'default parameters' do
-    it 'should run successfully' do
-      pp =<<-EOS
+    it 'runs successfully' do
+      pp = <<-EOS
       class { 'conman': }
       EOS
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe package('conman') do
-      it { should be_installed }
+      it { is_expected.to be_installed }
     end
 
     describe service('conman') do
-      it { should be_enabled }
-      it { should be_running }
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
     end
 
     describe file('/etc/conman.conf') do
-      it { should be_file }
-      it { should be_mode 644 }
-      it { should be_owned_by 'root' }
-      it { should be_grouped_into 'root' }
+      it { is_expected.to be_file }
+      it { is_expected.to be_mode 644 }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
     end
   end
 end
--- spec/classes/conman_spec.rb
+++ spec/classes/conman_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
 describe 'conman' do
-  on_supported_os({
-    :supported_os => [
-      {
-        "operatingsystem" => "RedHat",
-        "operatingsystemrelease" => ["6", "7"],
-      }
-    ]
-  }).each do |os, facts|
+  on_supported_os(supported_os: [
+                    {
+                      'operatingsystem' => 'RedHat',
+                      'operatingsystemrelease' => ['6', '7'],
+                    },
+                  ]).each do |os, facts|
     context "on #{os}" do
       let(:facts) do
         facts
@@ -20,48 +18,39 @@ describe 'conman' do
       it { is_expected.to contain_class('conman::params') }
 
       it do
-        is_expected.to contain_package('conman').only_with({
-          :ensure => 'present',
-          :name   => 'conman',
-        })
+        is_expected.to contain_package('conman').only_with(ensure: 'present',
+                                                           name: 'conman')
       end
 
       it do
-        is_expected.to contain_concat('/etc/conman.conf').with({
-          :ensure  => 'present',
-          :owner   => 'root',
-          :group   => 'root',
-          :mode    => '0644',
-          :require => 'Package[conman]'
-        })
+        is_expected.to contain_concat('/etc/conman.conf').with(ensure: 'present',
+                                                               owner: 'root',
+                                                               group: 'root',
+                                                               mode: '0640',
+                                                               require: 'Package[conman]')
       end
 
       it do
-        is_expected.to contain_service('conman').only_with({
-          :ensure      => 'running',
-          :enable      => 'true',
-          :name        => 'conman',
-          :hasstatus   => 'true',
-          :hasrestart  => 'true',
-          :subscribe   => 'Concat[/etc/conman.conf]',
-        })
+        is_expected.to contain_service('conman').only_with(ensure: 'running',
+                                                           enable: 'true',
+                                                           name: 'conman',
+                                                           hasstatus: 'true',
+                                                           hasrestart: 'true',
+                                                           subscribe: 'Concat[/etc/conman.conf]')
       end
 
       context 'when server => false' do
-        let(:params) {{ :server => false }}
+        let(:params) { { server: false } }
 
         it { is_expected.to contain_package('conman') }
         it { is_expected.not_to contain_concat('/etc/conman.conf') }
         it do
-          is_expected.to contain_service('conman').only_with({
-            :ensure  => 'stopped',
-            :enable  => 'false',
-            :name    => 'conman',
-            :require => 'Package[conman]',
-          })
+          is_expected.to contain_service('conman').only_with(ensure: 'stopped',
+                                                             enable: 'false',
+                                                             name: 'conman',
+                                                             require: 'Package[conman]')
         end
       end
-
     end # end context
   end # end on_supported_os loop
 end # end describe
